8f36a9c61ac93ea740571b98ae3a059b8caee5b1
deeuu <deeuu@users.noreply.github.com>
2017-09-19 15:30:36 +0100

    Latest mods from listen

--- assets/js/_mushra.js
+++ assets/js/_mushra.js
@@ -338,32 +338,12 @@ Mushra.prototype.complete = function()
 
     if (this.config.allow_submission)
     {
-        // Append inputs to the form
-        $('<input>').attr({
-                type: 'hidden',
-                name: 'fields[data]',
-                value: values,
-            }).appendTo ('div.submit-popup > form');
-
-        $('<input>').attr({
-                type: 'hidden',
-                name: 'fields[sounds]',
-                value: sounds,
-            }).appendTo ('div.submit-popup > form');
-
-        $('<input>').attr({
-                type: 'hidden',
-                name: 'fields[pages]',
-                value: pages,
-            }).appendTo ('div.submit-popup > form');
-
-        $('<input>').attr({
-                type: 'hidden',
-                name: 'fields[page_order]',
-                value: pageOrder,
-            }).appendTo ('div.submit-popup > form');
-
-        $activePage ('.submit-popup').popup('open');
+        $activePage ('input[name="fields[data]"]').val (values);
+        $activePage ('input[name="fields[sounds]"]').val (sounds);
+        $activePage ('input[name="fields[pages]"]').val (pages);
+        $activePage ('input[name="fields[page_order]"]').val (pageOrder);
+        $activePage ('input[name="fields[page_response_time]"]').val (times);
+        $activePage ('.submit-popup').popup ('open');
     }
     else{
         $.mobile.changePage (this.config.nextURL);
