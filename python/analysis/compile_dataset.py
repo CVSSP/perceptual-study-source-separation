c3dc2084bc01e0313a2c955a8c06eb3cbacedf97
deeuu <deeuu@users.noreply.github.com>
2017-09-27 13:55:01 +0100

    Initial script for compiling the dataset

--- python/analysis/compile_dataset.py
+++ python/analysis/compile_dataset.py
@@ -2,11 +2,13 @@
 Script to update the submissions to conform to expected data keys used by
 listen.
 '''
-import os
+import pandas as pd
 import listen
-import pandas
 
+frame = listen.parser.MUSHRA('./site/_data/results/interferer').parse()
 
-parser = listen.parser.MUSHRA('./site/_data/results/interferer')
-frame = parser.parse()
-#frame = pd.DataFrame('./site/_data/results/quality')
+frame = frame.append(
+    listen.parser.MUSHRA('./site/_data/results/quality').parse()
+)
+
+print(pd.unique(frame.subject))
