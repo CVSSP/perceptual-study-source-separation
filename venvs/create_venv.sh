bed966da3b92bf03eb41e254dc7b45773df0ef41
deeuu <deeuu@users.noreply.github.com>
2017-11-15 17:38:41 +0000

    Add commit hashes for our development projects

--- venvs/create_venv.sh
+++ venvs/create_venv.sh
@@ -26,16 +26,19 @@ ipython kernel install --prefix="$1" --name=$1
 
 git clone -b dev https://github.com/IoSR-Surrey/untwist.git
 cd untwist
+git checkout 08acb17c38358f88193fa15f4378e03e89c043f5
 python setup.py install
 cd ../
 
 git clone https://github.com/CVSSP/masseval.git
 cd masseval
+git checkout 6bb5b9b082e3f12bf3bb288c5ecca9d31805f212
 python setup.py install
 cd ../
 
 git clone https://github.com/CVSSP/massdatasets.git
 cd massdatasets
+git checkout ff3ff1574c0898422ee0849bbdd850c15015ff4b
 python setup.py install
 cd ../
 
@@ -47,6 +50,7 @@ cd ../
 git clone https://github.com/deeuu/listen
 cd listen
 git checkout python
+git checkout 671cbf3bedf1657eff44b053a7f5b184b21dda80
 cd python
 python setup.py install
 cd ../../
